36420f9fe3290e12d4d860692645923e2c530025
twilight <skyhighhl@gmail.com>
2021-10-13 02:09:19 +0900

    algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.kr.test.MathBasic1;
+
 import java.io.*;
 
 
@@ -5,32 +7,15 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         try {
-            int sum = 1;
-            int result = 0;
-            int nTmp = 1000000000/6;
-            String s="";
+            String s = br.readLine();
+            if ("2292".equals(s)) {
+                MathBasic1 m = new MathBasic1();
+                System.out.println(m.no2HoneyComb(br));
+            }
 
-                s = br.readLine();
-                int roomNo = Integer.parseInt(s);
-
-                if( roomNo <1 || roomNo >1000000000 )
-                    throw new Exception("out of range");
-                if( roomNo== 1) {
-                    result = 1;
-                }else {
-                    for (int i = 1; i < nTmp; i++) {
-                        sum += 6 * i;
-
-                        if (sum >= roomNo) {
-                            result = i + 1;
-                            break;
-                        }
-                    }
-                }
-                System.out.println( result );
-
-        } catch (Exception e) {
+        }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
